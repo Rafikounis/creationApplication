7773843e7029cf0aa88de7583d49ef5d3c2cf2de
Rafik <ounisrafik18@gmail.com>
2020-08-28 14:43:31 +0200

    Updates

---
+++ precache-manifest.a60e523f3628bfa96b252a4dd5081b19.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9360418af2f3b85730678f7d597e6c",
+    "url": "/creationApplication/index.html"
+  },
+  {
+    "revision": "e62e372ad0212862fc59",
+    "url": "/creationApplication/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "930b8fdf96234052ff6e",
+    "url": "/creationApplication/static/css/main.9f8d4b98.chunk.css"
+  },
+  {
+    "revision": "e62e372ad0212862fc59",
+    "url": "/creationApplication/static/js/2.c06686b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/creationApplication/static/js/2.c06686b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930b8fdf96234052ff6e",
+    "url": "/creationApplication/static/js/main.c37f4d9e.chunk.js"
+  },
+  {
+    "revision": "8646d1b1734775d4ea3b",
+    "url": "/creationApplication/static/js/runtime-main.34f98de7.js"
+  },
+  {
+    "revision": "555a5b6dfc431fae015cae30a6d4b00e",
+    "url": "/creationApplication/static/media/Clic1 Plat.555a5b6d.png"
+  },
+  {
+    "revision": "56e06fc89fbf312a0963c38e0c82b6cc",
+    "url": "/creationApplication/static/media/application 1.56e06fc8.jpg"
+  },
+  {
+    "revision": "30cc96f699148724868d79843e931a60",
+    "url": "/creationApplication/static/media/creation application ios.30cc96f6.jpg"
+  },
+  {
+    "revision": "bd4784a9568579c65da4986b8f3520c1",
+    "url": "/creationApplication/static/media/creation application.bd4784a9.jpg"
+  },
+  {
+    "revision": "bda3f765647a1f5b233ea12caf9bac2b",
+    "url": "/creationApplication/static/media/creation site web.bda3f765.jpg"
+  },
+  {
+    "revision": "5cceb7b06e62d0b7091efc4b63a9a968",
+    "url": "/creationApplication/static/media/mac.5cceb7b0.jpg"
+  }
+]);
